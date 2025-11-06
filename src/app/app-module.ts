dc107389f54e23f5cdc73589cf1a845221608c67
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-11-06 14:25:51 +0100

    inicio Comics

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -9,10 +9,23 @@ import { HijoCocheComponent } from './components/hijo.coche.component/hijo.coche
 import { PadreCochesComponent } from './components/padre.coches.component/padre.coches.component';
 import { PadreDeportesComponent } from './components/padre.deportes.component/padre.deportes.component';
 import { HijoDeporteComponent } from './components/hijo.deporte.component/hijo.deporte.component';
+import { ComicComponent } from './components/comic.component/comic.component';
+import { LibreriaComponent } from './components/libreria.component/libreria.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [App, ListaProductosComponent, MenuComponent, HijoCocheComponent, PadreCochesComponent, PadreDeportesComponent, HijoDeporteComponent],
-  imports: [BrowserModule, routing],
+  declarations: [
+    App,
+    ListaProductosComponent,
+    MenuComponent,
+    HijoCocheComponent,
+    PadreCochesComponent,
+    PadreDeportesComponent,
+    HijoDeporteComponent,
+    ComicComponent,
+    LibreriaComponent,
+  ],
+  imports: [BrowserModule, routing, FormsModule],
   providers: [provideBrowserGlobalErrorListeners(), appRoutingProvider],
   bootstrap: [App],
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,11 +4,13 @@ import { ListaProductosComponent } from './components/lista.productos.component/
 import { HijoCocheComponent } from './components/hijo.coche.component/hijo.coche.component';
 import { PadreCochesComponent } from './components/padre.coches.component/padre.coches.component';
 import { PadreDeportesComponent } from './components/padre.deportes.component/padre.deportes.component';
+import { LibreriaComponent } from './components/libreria.component/libreria.component';
 
 const appRoutes: Routes = [
   { path: '', component: ListaProductosComponent },
   { path: 'padrecoches', component: PadreCochesComponent },
   { path: 'padredeportes', component: PadreDeportesComponent },
+  { path: 'libreria', component: LibreriaComponent },
 ];
 
 export const appRoutingProvider: any[] = [];
